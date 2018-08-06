f636964225d4440a9574275e6cdc82f1ea18c752
aaljazza <aaljazza@gmail.com>
2018-08-06 18:01:10 +0300

    updated map function to access author array instead of HTML

--- src/App.js
+++ src/App.js
@@ -1,4 +1,31 @@
 import React, { Component } from "react";
+import authors from "./data";
+
+function authorInfo(author) {
+  return (
+    <div className="col-lg-4 col-md-6 col-12">
+      <div className="card">
+        <div className="image">
+          <img
+            className="card-img-top img-fluid"
+            src={author.imageUrl}
+            alt={author.first_name + "" + author.last_name}
+          />
+        </div>
+        <div className="card-body">
+          <h5 className="card-title">
+            <span>
+              {author.first_name} {author.last_name}
+            </span>
+          </h5>
+          <small className="card-text">{author.books.length} books</small>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+const authorList = authors.map(author => authorInfo(author));
 
 class App extends Component {
   render() {
@@ -19,59 +46,7 @@ class App extends Component {
             <div className="content col-10">
               <div className="authors">
                 <h3>Authors</h3>
-                <div className="row">
-                  <div className="col-lg-4 col-md-6 col-12">
-                    <div className="card">
-                      <div className="image">
-                        <img
-                          className="card-img-top img-fluid"
-                          src="https://nation.com.pk/digital_images/large/2015-05-10/jk-rowling-believes-in-standing-up-to-bullies-1431261222-3596.jpeg"
-                          alt="J.K. Rowling"
-                        />
-                      </div>
-                      <div className="card-body">
-                        <h5 className="card-title">
-                          <span>J.K. Rowling</span>
-                        </h5>
-                        <small className="card-text">7 books</small>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="col-lg-4 col-md-6 col-12">
-                    <div className="card">
-                      <div className="image">
-                        <img
-                          className="card-img-top img-fluid"
-                          src="https://typeset-beta.imgix.net/elite-daily/2016/03/06144207/elite-daily-George-RR-Martin.jpg?w=800&h=800&auto=format&q=70&fit=crop&crop=faces"
-                          alt="George R.R. Martin"
-                        />
-                      </div>
-                      <div className="card-body">
-                        <h5 className="card-title">
-                          <span>George R.R. Martin</span>
-                        </h5>
-                        <small className="card-text">5 books</small>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="col-lg-4 col-md-6 col-12">
-                    <div className="card">
-                      <div className="image">
-                        <img
-                          className="card-img-top img-fluid"
-                          src="https://images.8tracks.com/cover/i/010/010/920/tolkeinbigger-2362.jpg?rect=166,0,500,500&q=98&fm=jpg&fit=max"
-                          alt="J.R.R Tolkein"
-                        />
-                      </div>
-                      <div className="card-body">
-                        <h5 className="card-title">
-                          <span>J.R.R Tolkein</span>
-                        </h5>
-                        <small className="card-text">6 books</small>
-                      </div>
-                    </div>
-                  </div>
-                </div>
+                <div className="row">{authorList}</div>
               </div>
             </div>
           </div>
